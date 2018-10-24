a125b5d84b5f492248ef3c648c136b15c2564bd3
Matthew <m.c.langford@live.com>
2018-10-24 15:26:31 -0600

    initial commit

---
+++ src/components/rigListComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+
+
+class rigListComponent extends Component{
+    render() {
+        return (
+        <div>Hi there</div>
+        );
+    }
+}
+
+
+export default rigListComponent;
